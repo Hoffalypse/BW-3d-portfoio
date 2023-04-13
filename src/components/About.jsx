68d95127960a4d188a267cc363284d552f83d867
Bryan W <bhoff1980@gmail.com>
2023-04-13 03:38:44 -0600

    started workking through about section and added premade and additional styling

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import Tilt from 'react-parallax-tilt'
+import {motion} from 'framer-motion'
+import { styles } from '../styles'
+import { services } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.div variants={textVariant()}>
+      <p className={styles.sectionSubText}>Introduction</p>
+      <h2 className={styles.sectionHeadText}>Overview</h2>
+    </motion.div>
+    <motion.p 
+    variants={fadeIn('', '', 0.1, 1)}
+    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+    >I am a skilled software developer with experience in Typescript and Javascript and expertise in frameworks like React, Angular, Node.js and MongoDb. I am a fast learner and accel in problem solving. I colloaborate closely with clients to create efficient, scalable and user-friendly solutions that solve real world problems. Let's work together to bring your ideas to life. 
+
+    </motion.p>
+    </>
   )
 }
 
