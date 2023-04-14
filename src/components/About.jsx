83b696fcbb92e037f166c7073fad4ee62472d902
Bryan W <bhoff1980@gmail.com>
2023-04-14 00:18:33 -0600

    tilt functionality working and full card is rendering in about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,6 +4,7 @@ import {motion} from 'framer-motion'
 import { styles } from '../styles'
 import { services } from '../constants'
 import { fadeIn, textVariant } from '../utils/motion'
+import { SectionWrapper } from '../hoc';
 
 const ServiceCard =({index, title, icon}) => {
   return(
@@ -13,7 +14,20 @@ const ServiceCard =({index, title, icon}) => {
   variants={fadeIn("right", 'spring', .5 * index, .75)}
   className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
   >
-    <div> </div>
+    <div
+    options={{
+      max:45,
+      scale:1,
+      speed:450
+    }}
+    className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] justify-evenly items-center flex-col'
+    > 
+    <img 
+    src={icon} alt={title}
+    className='w-16 h-16 mb-10 mt-10 ml-10 object-contain'
+    />
+    <h3 className='text-white text-[20px] font-bold text-center'>{title}</h3>
+    </div>
 
   </motion.div>
 
@@ -46,4 +60,4 @@ const About = () => {
   )
 }
 
-export default About
+export default SectionWrapper(About, "about")
